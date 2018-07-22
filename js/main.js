7b475da6db15296acde730cdc380c40f76b71b8d
Gabriella-Karacsonyi <gabi.karacsonyi@gmail.com>
2018-07-22 20:25:13 +0200

    feat: Update landing page css with flexbox css rules
    Updates <main.js>, too, to meet a certain css change.
    The text content of the restaurant list items are created
    within a holding container.

--- js/main.js
+++ js/main.js
@@ -163,22 +163,28 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  //creates an extra container for he text below the restaurant image
+  // this chnage was needed because of a css update
+  const snippet = document.createElement('article');
+  snippet.className = 'restaurant-snippet';
+  li.append(snippet);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  snippet.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  snippet.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  snippet.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  snippet.append(more)
 
   return li
 }
